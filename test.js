de249537b83d21a44e058c011aeb96ad9888ef6b
damonryon <damon.ryon@gmail.com>
2024-04-30 08:33:14 -0700

    done

--- test.js
+++ test.js
@@ -1,1 +1,30 @@
-// Your code here 
+const Employee = require('./employee')
+const empReq = require('./employee')
+
+
+const johnW = new Employee('John Wick', 'Dog Lover')
+
+// setTimeout(() => {
+//     johnW.sayName()}, 2000)
+
+// let sayJohn = sayName.bind(johnW)
+
+// setTimeout(() => {
+//     sayJohn
+// }, 2000)
+
+
+let func = johnW.sayName
+
+let bound = func.bind(johnW)
+
+setTimeout(() => {
+    bound()
+}, 2000)
+
+let func2 = johnW.sayOccupation
+let bound2 = func2.bind(johnW)
+
+setTimeout(() => {
+    bound2()
+}, 3000)
